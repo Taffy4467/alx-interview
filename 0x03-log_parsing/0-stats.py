82db107a6ce0c4cd07c698a17bdf7edbfc39376b
Tafadzwa <gandarit4467@gmail.com>
2023-07-20 16:28:26 +0000

    stats.py redo

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,44 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
+"""Log Parser"""
 import sys
 
-def print_stats(total_file_size, status_code_count):
-    print("File size: {}".format(total_file_size))
-    for status_code in sorted(status_code_count.keys()):
-        count = status_code_count[status_code]
-        print("{}: {}".format(status_code, count))
 
-def parse_log_line(line):
-    parts = line.strip().split(" ")
-    if len(parts) != 9:
-        return None
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
-    ip, _, _, timestamp, _, request, status_code, file_size, _ = parts
-    if request != '"GET /projects/260 HTTP/1.1"':
-        return None
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
+    def parse_line(line):
+        """ Checks the line for matches """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
+    linenum = 1
     try:
-        status_code = int(status_code)
-        file_size = int(file_size)
-        return status_code, file_size
-    except ValueError:
-        return None
-
-def main():
-    total_file_size = 0
-    status_code_count = {}
-
-    try:
-        line_count = 0
         for line in sys.stdin:
-            log_entry = parse_log_line(line)
-            if log_entry:
-                status_code, file_size = log_entry
-                total_file_size += file_size
-                status_code_count[status_code] = status_code_count.get(status_code, 0) + 1
-
-            line_count += 1
-            if line_count == 10:
-                print_stats(total_file_size, status_code_count)
-                line_count = 0
-
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        print_stats(total_file_size, status_code_count)
-
-if __name__ == "__main__":
-    main()
-
+        print_stats()
+        raise
+    print_stats()

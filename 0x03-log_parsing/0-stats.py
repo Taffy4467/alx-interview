6370694434ea75802c356bb057effbc1aece2188
Tafadzwa <gandarit4467@gmail.com>
2023-07-20 07:43:15 +0000

    review stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,50 @@
 #!/usr/bin/python3
 '''a script that reads stdin line by line and computes metrics'''
-
-
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+def print_stats(total_file_size, status_code_count):
+    print("File size: {}".format(total_file_size))
+    for status_code in sorted(status_code_count.keys()):
+        count = status_code_count[status_code]
+        print("{}: {}".format(status_code, count))
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+def parse_log_line(line):
+    parts = line.strip().split(" ")
+    if len(parts) != 9:
+        return None
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    ip, _, _, timestamp, _, request, status_code, file_size, _ = parts
+    if request != '"GET /projects/260 HTTP/1.1"':
+        return None
 
-except Exception as err:
-    pass
+    try:
+        status_code = int(status_code)
+        file_size = int(file_size)
+        return status_code, file_size
+    except ValueError:
+        return None
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+def main():
+    total_file_size = 0
+    status_code_count = {}
+
+    try:
+        line_count = 0
+        for line in sys.stdin:
+            log_entry = parse_log_line(line)
+            if log_entry:
+                status_code, file_size = log_entry
+                total_file_size += file_size
+                status_code_count[status_code] = status_code_count.get(status_code, 0) + 1
+
+            line_count += 1
+            if line_count == 10:
+                print_stats(total_file_size, status_code_count)
+                line_count = 0
+
+    except KeyboardInterrupt:
+        print_stats(total_file_size, status_code_count)
+
+if __name__ == "__main__":
+    main()
+

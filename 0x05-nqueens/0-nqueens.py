669d441facf67f93afe4e41825e0423b7e04c1a7
Tafadzwa <gandarit4467@gmail.com>
2023-08-01 19:28:21 +0000

    updating code

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
 '''N Queens Challenge'''
 
-def checks(board, row, col):
+import sys
 
-    for i in range(col):
-        if board[i] is row or abs(board[i] - row) is abs(i - col):
-            return False
-    return True
+def is_safe(board, row, col):
+  for i in range(row):
+    if board[i][col] == 1:
+      return False
+  for i in range(row):
+    for j in range(col):
+      if board[i][j] == 1 and (row - i) == (col - j) or (row - i) == (j - col):
+        return False
+  return True
 
+def solve_nqueens(board, row, n):
+  if row == n:
+    print(board)
+    return
+  for col in range(n):
+    if is_safe(board, row, col):
+      board[row][col] = 1
+      solve_nqueens(board, row + 1, n)
+      board[row][col] = 0
 
-def cboard(board, col):
-
-    size = len(board)
-    if col is size:
-        print(str([[i, board[i]] for i in range(size)]))
-        return
-    for n in range(size):
-        if checks(board, n, col):
-            board[col] = n
-            cboard(board, col + 1)
-
+def main():
+  if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+  try:
+    n = int(sys.argv[1])
+  except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+  if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+  board = [[0] * n for _ in range(n)]
+  solve_nqueens(board, 0, n)
 
 if __name__ == "__main__":
-    import sys
+  main()
 
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    n = 0
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    board = [0 for col in range(n)]
-    cboard(board, 0)
